3ab8c5235bc7f370af0c0639f5459bc10cea3311
Richard <richard@rghsoftware.co.uk>
2023-06-14 10:41:50 +0100

    example describing images

---
+++ describe.py
@@ -0,0 +1,35 @@
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+
+def classify(images):
+    pixel_values = feature_extractor(images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+    from PIL import Image
+
+    image = Path(__file__).parent / "memories" / "2022-06-03" / "primary.jpg"
+    image = Image.open(image)
+    print(classify([image]))
