f673555f66170e9a56acca0b5c93d8c4356f8a9b
Richard <richard@rghsoftware.co.uk>
2023-06-14 11:00:32 +0100

    writing out all images with descriptions

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from pathlib import Path
 from bereal_gpt.memories import Memory
 from bereal_gpt.described_memory import DescribedMemory
 
-memories = Memory.from_directory(Path(__file__).parent / "memories")
-for memory in memories[:10]:
-    print(DescribedMemory(memory).primary_description())
+directory = Path(__file__).parent
+described_directory = directory / "described"
+
+memories = Memory.from_directory(directory / "memories")
+
+for memory in memories:
+    print(memory.memory_day())
+    described = DescribedMemory(memory)
+
+    memory.primary_image().save(described_directory / f"{described.primary_description()}.jpg")
+    memory.secondary_image().save(described_directory / f"{described.secondary_description()}.jpg")
