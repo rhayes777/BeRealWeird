394c86e205837382a9164953adcf2e7a9c043764
Richard <richard@rghsoftware.co.uk>
2023-06-14 10:51:13 +0100

    take the first 10

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from bereal_gpt.memories import Memory
 from bereal_gpt.describe import describe
 
 memories = Memory.from_directory(Path(__file__).parent / "memories")
-for memory in memories[-10:]:
+for memory in memories[:10]:
     print(memory.memory_day(), describe([memory.primary_image()]))
